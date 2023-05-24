6ccc7da26c611319070378da5374eeb3f38a8fab
shinjaewon99 <tlswodnjs99@naver.com>
2023-05-24 17:24:26 +0900

    test : UserDao get() 메서드 기능 테스트 - Db 값 get() 테스트

--- src/test/java/com/example/likelion_spring/dao/UserDaoTest.java
+++ src/test/java/com/example/likelion_spring/dao/UserDaoTest.java
@@ -1,10 +1,12 @@
 package com.example.likelion_spring.dao;
 
 import com.example.likelion_spring.domain.User;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import java.sql.SQLException;
 
+import static org.assertj.core.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.*;
 
 class UserDaoTest {
@@ -18,6 +20,17 @@ class UserDaoTest {
         user.setPassword("1234");
 
         userDao.add(user);
+    }
+
+    @Test
+    void getName() throws SQLException, ClassNotFoundException {
+        UserDao userDao = new UserDao();
+        User result = userDao.get("0");
+
+        System.out.println(result.getId());
+        System.out.println(result.getName());
+
+        assertThat(result.getId()).isEqualTo("0");
     }
 
 }
