f9121406712f8059c13f39ed21b9391d2c19ad73
shinjaewon99 <tlswodnjs99@naver.com>
2023-05-24 18:21:01 +0900

    fix : 클래스 이름 변경

---
+++ src/main/java/com/example/likelion_spring/dao/SimpleConnectionMaker.java
@@ -0,0 +1,14 @@
+package com.example.likelion_spring.dao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class SimpleConnectionMaker {
+    public Connection makeConnection() throws ClassNotFoundException, SQLException {
+        Class.forName("com.mysql.cj.jdbc.Driver");
+        Connection c = DriverManager.getConnection(
+                "jdbc:mysql://ec2-3-38-40-246.ap-northeast-2.compute.amazonaws.com/springbook", "root", "password");
+        return c;
+    }
+}
