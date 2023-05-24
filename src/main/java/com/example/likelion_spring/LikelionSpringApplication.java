9b5f10ee80f4bbdceeacd9bfcf0ef188d186a8c3
shinjaewon99 <tlswodnjs99@naver.com>
2023-05-24 16:37:20 +0900

    feat : build 정보 추가

---
+++ src/main/java/com/example/likelion_spring/LikelionSpringApplication.java
@@ -0,0 +1,13 @@
+package com.example.likelion_spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LikelionSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LikelionSpringApplication.class, args);
+    }
+
+}
